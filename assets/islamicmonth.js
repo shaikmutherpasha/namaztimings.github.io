41b63705f905bfcad2e8c1129b009213d914a44d
shaikmutherpasha <shaikmutherpasha@users.noreply.github.com>
2017-02-05 01:52:13 +0530

    Update islamicmonth.js

--- assets/islamicmonth.js
+++ assets/islamicmonth.js
@@ -1,98 +1,98 @@
-function gmod(n,m){
- return ((n%m)+m)%m;
+function gmod(n, m) {
+    return ((n % m) + m) % m;
 }
 
-function kuwaiticalendar(adjust){
- var today = new Date();
- if(adjust) {
-  adjustmili = 1000*60*60*24*adjust;
-  todaymili = today.getTime()+adjustmili;
-  today = new Date(todaymili);
- }
- day = today.getDate();
- month = today.getMonth();
- year = today.getFullYear();
- m = month+1;
- y = year;
- if(m<3) {
-  y -= 1;
-  m += 12;
- }
+function kuwaiticalendar(adjust) {
+    var today = new Date();
+    if (adjust) {
+        adjustmili = 1000 * 60 * 60 * 24 * adjust;
+        todaymili = today.getTime() + adjustmili;
+        today = new Date(todaymili);
+    }
+    day = today.getDate();
+    month = today.getMonth();
+    year = today.getFullYear();
+    m = month + 1;
+    y = year;
+    if (m < 3) {
+        y -= 1;
+        m += 12;
+    }
 
- a = Math.floor(y/100.);
- b = 2-a+Math.floor(a/4.);
- if(y<1583) b = 0;
- if(y==1582) {
-  if(m>10)  b = -10;
-  if(m==10) {
-   b = 0;
-   if(day>4) b = -10;
-  }
- }
+    a = Math.floor(y / 100.);
+    b = 2 - a + Math.floor(a / 4.);
+    if (y < 1583) b = 0;
+    if (y == 1582) {
+        if (m > 10) b = -10;
+        if (m == 10) {
+            b = 0;
+            if (day > 4) b = -10;
+        }
+    }
 
- jd = Math.floor(365.25*(y+4716))+Math.floor(30.6001*(m+1))+day+b-1524;
+    jd = Math.floor(365.25 * (y + 4716)) + Math.floor(30.6001 * (m + 1)) + day + b - 1524;
 
- b = 0;
- if(jd>2299160){
-  a = Math.floor((jd-1867216.25)/36524.25);
-  b = 1+a-Math.floor(a/4.);
- }
- bb = jd+b+1524;
- cc = Math.floor((bb-122.1)/365.25);
- dd = Math.floor(365.25*cc);
- ee = Math.floor((bb-dd)/30.6001);
- day =(bb-dd)-Math.floor(30.6001*ee);
- month = ee-1;
- if(ee>13) {
-  cc += 1;
-  month = ee-13;
- }
- year = cc-4716;
+    b = 0;
+    if (jd > 2299160) {
+        a = Math.floor((jd - 1867216.25) / 36524.25);
+        b = 1 + a - Math.floor(a / 4.);
+    }
+    bb = jd + b + 1524;
+    cc = Math.floor((bb - 122.1) / 365.25);
+    dd = Math.floor(365.25 * cc);
+    ee = Math.floor((bb - dd) / 30.6001);
+    day = (bb - dd) - Math.floor(30.6001 * ee);
+    month = ee - 1;
+    if (ee > 13) {
+        cc += 1;
+        month = ee - 13;
+    }
+    year = cc - 4716;
 
- if(adjust) {
-  wd = gmod(jd+1-adjust,7)+1;
- } else {
-  wd = gmod(jd+1,7)+1;
- }
+    if (adjust) {
+        wd = gmod(jd + 1 - adjust, 7) + 1;
+    } else {
+        wd = gmod(jd + 1, 7) + 1;
+    }
 
- iyear = 10631./30.;
- epochastro = 1948084;
- epochcivil = 1948085;
+    iyear = 10631. / 30.;
+    epochastro = 1948084;
+    epochcivil = 1948085;
 
- shift1 = 8.01/60.;
+    shift1 = 8.01 / 60.;
 
- z = jd-epochastro;
- cyc = Math.floor(z/10631.);
- z = z-10631*cyc;
- j = Math.floor((z-shift1)/iyear);
- iy = 30*cyc+j;
- z = z-Math.floor(j*iyear+shift1);
- im = Math.floor((z+28.5001)/29.5);
- if(im==13) im = 12;
- id = z-Math.floor(29.5001*im-29);
+    z = jd - epochastro;
+    cyc = Math.floor(z / 10631.);
+    z = z - 10631 * cyc;
+    j = Math.floor((z - shift1) / iyear);
+    iy = 30 * cyc + j;
+    z = z - Math.floor(j * iyear + shift1);
+    im = Math.floor((z + 28.5001) / 29.5);
+    if (im == 13) im = 12;
+    id = z - Math.floor(29.5001 * im - 29);
 
- var myRes = new Array(8);
+    var myRes = new Array(8);
 
- myRes[0] = day; //calculated day (CE)
- myRes[1] = month-1; //calculated month (CE)
- myRes[2] = year; //calculated year (CE)
- myRes[3] = jd-1; //julian day number
- myRes[4] = wd-1; //weekday number
- myRes[5] = id; //islamic date
- myRes[6] = im-1; //islamic month
- myRes[7] = iy; //islamic year
+    myRes[0] = day; //calculated day (CE)
+    myRes[1] = month - 1; //calculated month (CE)
+    myRes[2] = year; //calculated year (CE)
+    myRes[3] = jd - 1; //julian day number
+    myRes[4] = wd - 1; //weekday number
+    myRes[5] = id; //islamic date
+    myRes[6] = im - 1; //islamic month
+    myRes[7] = iy; //islamic year
 
- return myRes;
+    return myRes;
 }
+
 function writeIslamicDate(adjustment) {
- var wdNames = new Array("Ahad","Ithnin","Thulatha","Arbaa","Alkhamees","Jumuah","Sabt");
- var iMonthNames = new Array("Muharram","Safar","Rabi'ul Awwal","Rabi'ul Akhir",
- "Jumadal Ula","Jumadal Akhira","Rajab","Sha'ban",
- "Ramadan","Shawwal","Dhul Qa'ada","Dhul Hijja");
- var iDate = kuwaiticalendar(adjustment);
- var outputIslamicDate = wdNames[iDate[4]] + ", "
- + iDate[5] + " " + iMonthNames[iDate[6]] + " " + iDate[7] + " AH";
- return outputIslamicDate;
+    var wdNames = new Array("Ahad", "Ithnin", "Thulatha", "Arbaa", "Alkhamees", "Jumuah", "Sabt");
+    var iMonthNames = new Array("Muharram", "Safar", "Rabi'ul Awwal", "Rabi'ul Akhir",
+        "Jumadal Ula", "Jumadal Akhira", "Rajab", "Sha'ban",
+        "Ramadan", "Shawwal", "Dhul Qa'ada", "Dhul Hijja");
+    var iDate = kuwaiticalendar(adjustment);
+    var outputIslamicDate = wdNames[iDate[4]] + ", " + iDate[5] + " " + iMonthNames[iDate[6]] + " " + iDate[7] + " AH";
+    return outputIslamicDate;
 }
 
 var d = writeIslamicDate();
@@ -100,17 +100,17 @@ $('#total').html(d);
 
 var currentTime = new Date()
 var weekday = new Array(7);
-    weekday[0] = "Sunday";
-    weekday[1] = "Monday";
-    weekday[2] = "Tuesday";
-    weekday[3] = "Wednesday";
-    weekday[4] = "Thursday";
-    weekday[5] = "Friday";
-    weekday[6] = "Saturday";
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
 
 var day = weekday[currentTime.getDay()];
 var month = currentTime.getMonth() + 1
 var date = currentTime.getDate()
 var year = currentTime.getFullYear()
-$('#date').html(day + "  " + date  + "/" + month + "/" + year);
-$('#time').html((currentTime.getHours()<10?'0':'') + currentTime.getHours()  + ':' + (currentTime.getMinutes()<10?'0':'') + currentTime.getMinutes());
+$('#date').html(day + "  " + date + "/" + month + "/" + year);
+$('#time').html((currentTime.getHours() < 10 ? '0' : '') + currentTime.getHours() + ':' + (currentTime.getMinutes() < 10 ? '0' : '') + currentTime.getMinutes());
