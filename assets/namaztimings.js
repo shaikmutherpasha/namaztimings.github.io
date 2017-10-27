2fcfeb6ecea486cadbd221bf4cd679287341d023
shaikmutherpasha <shaikmutherpasha@users.noreply.github.com>
2017-10-27 12:53:33 +0530

    Update namaztimings.js

--- assets/namaztimings.js
+++ assets/namaztimings.js
@@ -19,4 +19,20 @@ $(document).ready(function(){
             }
         });
   })
+	
+	
+    var dateData = new Date();
+    var date = dateData.getDate()
+    var monthName = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    var month = monthName[dateData.getMonth()];
+    var year = dateData.getFullYear();
+    var fullDate = date + " " + month + " " + year;
+
+    if ($("tr:nth-child(27) th:first").text() == fullDate) {
+        $("tr:nth-child(27)").css("background-color": "#008d76", "color": "#fff");
+    }
+	
+	
+	
+	
 });
